fa0e4b14847004727a6a7c088e7ed17c87fcb2e3
duynguyen-workspace <duynguyen.workspace@gmail.com>
2024-01-16 11:26:55 +0700

    Update trash bin and trash scenes

---
+++ controllers/trash_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+const TRASH_LIST = [
+	preload("res://assets/Spritesheet/Trash/trash assets/alcohol 1.png"),
+	preload("res://assets/Spritesheet/Trash/trash assets/box 2.png"),
+	preload("res://assets/Spritesheet/Trash/trash assets/computer 1.png"),
+	preload("res://assets/Spritesheet/Trash/trash assets/dryer.png"),
+	preload("res://assets/Spritesheet/Trash/trash assets/garbage bag 1.png"),
+	preload("res://assets/Spritesheet/Trash/trash assets/screen 1.png"),
+	preload("res://assets/Spritesheet/Trash/trash assets/washer.png"),
+	preload("res://assets/Spritesheet/Trash/trash assets/water bottle dirty.png"),
+]
--- models/characters/player/player.gd
+++ models/characters/player/player.gd
@@ -13,6 +13,11 @@ enum PLAYER_STATE { IDLE, RUN, HURT }
 
 var _state: PLAYER_STATE = PLAYER_STATE.IDLE
 
+@onready var trash_pos = $TrashPos
+@onready var trash_detector = $TrashDetector
+var heldTrash = null
+const PICKUP_DISTANCE = 50
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -70,3 +75,34 @@ func _on_dash_cooldown_timeout():
 func _on_dash_time_timeout():
 	canDash = false
 	dash_cooldown.start()
+
+func try_pickup_trash():
+	# Get a list of bodies overlapping with the player
+	var bodies = trash_detector.get_overlapping_bodies()
+
+	# Check for trash objects in the collaping area
+	for body in bodies:
+		if body.is_in_group("trash"):
+			# Check if the trash is within the pickup distance
+			var distance_to_trash = position.distance_to(body.position)
+			if distance_to_trash < PICKUP_DISTANCE:
+				# Pick up the trash
+				pickup_trash(body)
+				return  # Only pick up one trash object at a time
+
+func pickup_trash(trash):
+	# Pick up the trash object
+	heldTrash = trash
+	trash.queue_free()  # Remove the trash from the scene
+	# Additional logic (e.g., update inventory) can be added here
+	update_trash_state()
+	
+func drop_object():
+	# Place the object at the player's position
+	heldTrash.position = position
+	add_child(heldTrash) 
+	heldTrash = null
+
+func update_trash_state():
+	if heldTrash == null:
+		return
